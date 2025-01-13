0b70ac3f2f92fc59d78f37fe53c869fe5efe5f5a
Tim <perfodio@yandex.ru>
2025-01-13 12:07:29 +0300

    Added ability to get config file via the command line

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -1,7 +1,12 @@
 #include "client.h"
 
-int main() {
-    Client client("configs/config_client.toml");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <path/to/config_file>"<< std::endl;
+        exit(1);
+    }
+    std::string config_file = argv[1];
+    Client client(config_file);
 
     return 0;
 }
--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,7 +1,12 @@
 #include "server.h"
 
-int main() {
-    Server server("configs/config_server.toml");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <path/to/config_file>"<< std::endl;
+        exit(1);
+    }
+    std::string config_file = argv[1];
+    Server server(config_file);
 
     return 0;
 }
